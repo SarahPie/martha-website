4ca0b15d55502d742a5f8925fda824de5954996d
Sarah A <43837799+SarahPie@users.noreply.github.com>
2021-05-05 15:21:29 -0700

    Update martha.php

--- martha.php
+++ martha.php
@@ -1,15 +1,16 @@
 <?php
-$user = "martha";
-$password = "martha12!scuapp";
-$host = "mysql.engr.scu.edu";
-$dbase = "sdb_martha";
-$table = "lessons";
+//some variables have been changed for privacy reasons
+$user = "x";
+$password = "x";
+$host = "x";
+$dbase = "x";
+$table = "x";
 $url = "https://martha.engr.scu.edu";
 $connection= new mysqli($host, $user, $password, $dbase);
  if ($connection->connect_error) {
  die("Connection failed: " . $connection->connect_error);
 } else {
-$sql = "SELECT lesson, filepath FROM lessons";
+$sql = "SELECT x, y FROM table";
 $result = $connection->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
